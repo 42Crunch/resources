1bb1a7c2e90c83a0172a4b5b8f4258102a1f4519
Pierrick Prévert <pierrick@42crunch.com>
2022-11-18 13:45:40 +0100

    Creation of a task definition template. Modification of the tutorial

--- firewall-deployment/aws-ecs-fargate-artifacts/create-aws-secrets.sh
+++ firewall-deployment/aws-ecs-fargate-artifacts/create-aws-secrets.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-# Create secrets
-echo "===========> Creating AWS Secrets "
-aws secretsmanager create-secret --name 42c-protection-token --secret-string `cat etc/secret-protection-token`
+# Retrieve environment variables
+. ./variables.env
+
+# Create the 42c-tutorial-protection-token secret in AWS Secrets Manager if it does not exist. Else, update it.
+
+aws secretsmanager describe-secret --secret-id 42c-tutorial-protection-token 2>/dev/null >/dev/null
+retVal=$?
+if [ $retVal -ne 0 ]; then
+	echo "===========> Creating AWS Secrets "
+	aws secretsmanager create-secret --name 42c-tutorial-protection-token --secret-string $XLIIC_PROTECTION_TOKEN
+else
+	echo "===========> Updating 42c-tutorial-protection-token AWS secret "
+	aws secretsmanager update-secret --secret-id 42c-tutorial-protection-token --secret-string $XLIIC_PROTECTION_TOKEN
+fi
+
+
